8886cb1275ecabb4375479b0346fd70f88ecc9cf
nfl-nzr <nowfal_n@yahoo.com>
2024-01-24 18:56:13 +0530

    feat: added initial tf files

---
+++ nodes/providers.tf
@@ -0,0 +1,3 @@
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+}
---
+++ nodes/variables.tf
@@ -0,0 +1,4 @@
+variable "pm_api_url" {
+    type        = string
+    description = "API URL of the proxmox instance to be managed"
+}
