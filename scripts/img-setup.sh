64a2f8498272236a262ed478045208165c272408
nfl-nzr <nowfal_n@yahoo.com>
2024-01-26 23:34:31 +0530

    feat: updated cloud image script
    added support for qm agent and deafult users using virt-customize.
    
    none

--- scripts/img-setup.sh
+++ scripts/img-setup.sh
@@ -1,31 +1,49 @@
+apt update -y && apt install libguestfs-tools -y
+
+# Download image
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 
-# Create a VM
-qm create 9000 --name nfl.ubuntu2204.cloud.image --cpu host --cores 2 --sockets 1 --memory 2048 --net0 virtio,bridge=vmbr1
+# Install qemu-guest-agent on the image. Additional packages can be specified by separating with a comma.
+virt-customize -a jammy-server-cloudimg-amd64.img --install qemu-guest-agent
 
-# Import the disk in qcow2 format (as unused disk) 
+# Read and set root user password. TODO move passwd to file
+virt-customize -a jammy-server-cloudimg-amd64.img --root-password password:0000
+
+# Create an additional user.
+virt-customize -a jammy-server-cloudimg-amd64.img --run-command "useradd -m -s /bin/bash nfx"
+
+# Set password for that user.
+virt-customize -a jammy-server-cloudimg-amd64.img --password nfx:password:0000
+
+# Delete temporary password files safely.
+# shred -uv password_root.txt password_myuser.txt
+
+# Update all packages in the image.
+virt-customize -a jammy-server-cloudimg-amd64.img --update
+
+# Create VM
+qm create 9000 --name "ubuntu-20.04-template" --memory 2048 --cores 2 --cpu host --sockets 1 --net0 virtio,bridge=vmbr0
+
+# Import disk in qcow2 format
 qm importdisk 9000 jammy-server-cloudimg-amd64.img storage -format qcow2
 
-# Attach the disk to the vm using VirtIO SCSI
-qm set 9000 --scsihw virtio-scsi-pci --scsi0 /mnt/pve/storage/images/9000/vm-9000-disk-0.qcow2
+# Configure disks
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 storage:9000/vm-9000-disk-0.qcow2
 
-# Important settings
-qm set 9000 --ide2 storage:cloudinit --boot c --bootdisk scsi0 --serial0 socket --vga serial0
+# Resize does not work without this. Bug?
+sleep 20
 
-# The initial disk is only 2GB, thus we make it larger
+# Resize as the initial disk is 2G
 qm resize 9000 scsi0 +15G
 
-# Using static IP
-qm set 9000 --ipconfig0 ip=192.168.1.60/24,gw=192.168.1.1
+qm set 9000 --boot c --bootdisk scsi0
 
-# user authentication for 'ubuntu' user (optional password)
-qm set 9000 --cipassword 0000
+qm set 9000 --ide2 storage:cloudinit
+qm set 9000 --serial0 socket --vga serial0
 
-# check the cloud-init config
-qm cloudinit dump 9000 user
+# Enable qemu agent
+qm set 9000 --agent enabled=1
 
-# create tempalte and a linked clone
 qm template 9000
 
-# qm clone 9000 190 --name master-01
-# qm start 190
+rm jammy-server-cloudimg-amd64.img
